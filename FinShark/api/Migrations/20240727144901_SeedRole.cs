e572a97f3dea92a3de8dce48729030b798b928e1
Ofir Adany <ofiradany@gmail.com>
2024-07-27 21:14:38 +0300

    before deleting the db

---
+++ FinShark/api/Migrations/20240727144901_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3a0ac2e5-a1aa-4f91-ba73-40aa8033fd4e", null, "Admin", "ADMIN" },
+                    { "70e43c7e-8a58-445d-82c1-cb2adf3bf927", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3a0ac2e5-a1aa-4f91-ba73-40aa8033fd4e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "70e43c7e-8a58-445d-82c1-cb2adf3bf927");
+        }
+    }
+}
