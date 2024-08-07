9df82d204c8f7f350a992080eb0b17a7c9ddd9cb
Ofir Adany <ofiradany@gmail.com>
2024-08-07 11:18:33 +0300

    progressing! added more api calls

---
+++ FinShark/frontend/src/Components/IncomeStatement/IncomeStatement.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const IncomeStatement = (props: Props) => {
+  return <div> Income </div>;
+};
+
+export default IncomeStatement;
--- FinShark/frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ FinShark/frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,13 +1,43 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
 import { CompanyProfile } from "../../company";
+import { useParams } from "react-router-dom";
 import { getCompanyProfile } from "../../api";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Tile from "../../Components/Tile/Tile";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  const { ticker } = useParams();
+  let { ticker } = useParams();
+  const tabItems = [
+    {
+      id: 1,
+      title: "Company Profile",
+      icon: "fas fa-child",
+      content: "step 1 content",
+    },
+    {
+      id: 2,
+      title: "Income Statment",
+      icon: "fas fa-users",
+      content: "step 2 content",
+    },
+    {
+      id: 3,
+      title: "Balance Sheet",
+      icon: "fas fa-network-wired",
+      content: "step 3 content",
+    },
+    {
+      id: 4,
+      title: "Cash Flow",
+      icon: "fa money-check-alt",
+      content: "step 4 content",
+    },
+  ];
   const [company, setCompany] = useState<CompanyProfile>();
+  const [activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(() => {
     const getProfileInit = async () => {
@@ -20,9 +50,21 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard
+            tabItems={tabItems}
+            activeSidebarItem={activeSidebarItem!}
+            ticker={ticker!}
+          >
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="DCF" subTitle={company.dcf.toString()} />
+            <Tile title="Dividend" subTitle={company.lastDiv.toString()} />
+            <Tile title="Price" subTitle={company.price.toString()} />
+          </CompanyDashboard>
+        </div>
       ) : (
-        <div> Company not found! </div>
+        <div>Company Not Found!</div>
       )}
     </>
   );
